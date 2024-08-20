9c6cbc2f80612d612a6372649dac940a37416511
mohd-faisal07 <faisalahmed36950@gmail.com>
2024-08-21 02:41:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import Loader from "./Loader";
+
+function App() {
+  const [fetched, setFetched] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const apiKey = "https://stoic-quotes.com/api/quote";
+
+  const fetchQuote = useEffect(function () {
+    async function fetchget(url) {
+      try {
+        setIsLoading(true);
+        const response = await fetch(url);
+        var data = await response.json();
+        setFetched(data);
+        console.log(data);
+      } catch (error) {
+        throw new Error("error while fetching");
+      } finally {
+        setIsLoading(false);
+        console.log(isLoading);
+      }
+    }
+    fetchget(apiKey);
+  }, []);
+  fetchQuote;
+
+  function handleClick() {
+    window.location.reload();
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center bg-slate-500 h-screen">
+      <div className=" text-center m-5 p-5 bg-slate-800 border-solid rounded-xl border-slate-950">
+        <p className="text-xl text-blue-500 rounded-xl border-gray-400">
+          &#34;{isLoading ? <Loader /> : fetched.text}&#34;
+          {/* {fetched.text} */}
+        </p>
+        <p className="text-xl text-blue-300">
+          -{isLoading ? <Loader /> : fetched.author}
+          {/* -{fetched.author} */}
+        </p>
+      </div>
+      <button
+        onClick={() => handleClick()}
+        className=" text-black bg-slate-600 border-solid rounded-sm border-slate-950 p-2 m-5"
+      >
+        Regenerate
+      </button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Loader.jsx
@@ -0,0 +1,5 @@
+function Loader() {
+  return <p className="text-xl text-white  ">Loading...</p>;
+}
+
+export default Loader;
